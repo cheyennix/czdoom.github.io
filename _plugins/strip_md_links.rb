df2bdc05c94b404646dd867923d457366b1c00d6
Cheyenne <47585766+cheyennix@users.noreply.github.com>
2026-01-11 14:49:58 -0800

    image fix hopefully

--- _plugins/strip_md_links.rb
+++ _plugins/strip_md_links.rb
@@ -11,16 +11,27 @@ Jekyll::Hooks.register :documents, :pre_render do |document|
     link_path = "/#{link_path}" unless link_path.start_with?('/')
     
     # Remove redundant folder/file pattern (e.g., /cheyennix/cheyennix -> /cheyennix)
-    # This handles cases like cheyennix/cheyennix.md -> /cheyennix/
     link_path = link_path.sub(/\/([^\/]+)\/\1$/, '/\1')
     
     # Construct the link with trailing slash
     "[#{link_text}](#{link_path}/)"
   end
+  
+  # Convert image paths to absolute paths
+  document.content = document.content.gsub(/!\[([^\]]*)\]\(([^)]+?)\)/) do
+    alt_text = $1
+    image_path = $2
+    
+    # Skip if it's an external URL or already absolute
+    next $& if image_path =~ /^https?:\/\// || image_path.start_with?('/')
+    
+    # Add leading slash to make it absolute
+    "![#{alt_text}](/#{image_path})"
+  end
 end
 
 Jekyll::Hooks.register :pages, :pre_render do |page|
-  # Same conversion for pages
+  # Same conversion for pages - markdown links
   page.content = page.content.gsub(/\[([^\]]+)\]\(([^)]+?)\.md\)/) do
     link_text = $1
     link_path = $2
@@ -31,10 +42,22 @@ Jekyll::Hooks.register :pages, :pre_render do |page|
     # Add leading slash if not present
     link_path = "/#{link_path}" unless link_path.start_with?('/')
     
-    # Remove redundant folder/file pattern (e.g., /cheyennix/cheyennix -> /cheyennix)
+    # Remove redundant folder/file pattern
     link_path = link_path.sub(/\/([^\/]+)\/\1$/, '/\1')
     
     # Construct the link with trailing slash
     "[#{link_text}](#{link_path}/)"
+  end
+  
+  # Convert image paths to absolute paths
+  page.content = page.content.gsub(/!\[([^\]]*)\]\(([^)]+?)\)/) do
+    alt_text = $1
+    image_path = $2
+    
+    # Skip if it's an external URL or already absolute
+    next $& if image_path =~ /^https?:\/\// || image_path.start_with?('/')
+    
+    # Add leading slash to make it absolute
+    "![#{alt_text}](/#{image_path})"
   end
 end
--- zapathie/zapathie.md
+++ zapathie/zapathie.md
@@ -5,3 +5,6 @@ permalink: /zapathie/
 ---
 ![Zooble...](assets/images/zapathie_pfp.jpg)
 (zapathie goes here >:3)
+
+---
+![...](assets/images/lea_caramelldansen.gif)
